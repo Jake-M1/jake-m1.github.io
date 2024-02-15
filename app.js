273c0f7308b23ef4c843993d3a5fe8ba4fc63ff1
Jake-M1 <jsmulligan123@gmail.com>
2024-02-14 17:03:49 -0800

    Added project links

---
+++ app.js
@@ -0,0 +1,30 @@
+const projectButton1 = document.querySelector('#projectbutton1');
+const projectButton2 = document.querySelector('#projectbutton2');
+const projectButton3 = document.querySelector('#projectbutton3');
+const projectButton4 = document.querySelector('#projectbutton4');
+const projectButton5 = document.querySelector('#projectbutton5');
+const projectButton6 = document.querySelector('#projectbutton6');
+
+projectButton1.addEventListener('click', function() {
+    window.open('https://github.com/Jake-M1/SleepRecommendationApp', '_blank');
+});
+
+projectButton2.addEventListener('click', function() {
+    window.open('https://github.com/Jake-M1/Search-Engine', '_blank');
+});
+
+projectButton3.addEventListener('click', function() {
+    window.open('https://github.com/Jake-M1/PathTracer', '_blank');
+});
+
+projectButton4.addEventListener('click', function() {
+    window.open('https://github.com/Jake-M1/3DScene', '_blank');
+});
+
+projectButton5.addEventListener('click', function() {
+    window.open('https://github.com/Jake-M1/IceCubeClimber', '_blank');
+});
+
+projectButton6.addEventListener('click', function() {
+    window.open('https://github.com/Jake-M1/WebCrawler', '_blank');
+});
